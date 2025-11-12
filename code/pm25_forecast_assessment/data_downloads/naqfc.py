df631c6ed2b83ba1654638877c326db0cf513e38
sarahfwang <sfwang00@gmail.com>
2025-11-12 15:21:00 -0500

    bug: naqfc data download no valid key

--- code/pm25_forecast_assessment/data_downloads/naqfc.py
+++ code/pm25_forecast_assessment/data_downloads/naqfc.py
@@ -8,23 +8,53 @@ import pandas as pd
 import requests
 import xarray as xr
 
-def create_naqfc_url_and_get_xr(day: datetime.date, cycle: int):
+
+def get_AQM_version(day: datetime.date) -> str:
+    if day.year < 2020:
+        raise ValueError(f"naqfc doesn't support years < 2020. Year given: {day.year}")
+    elif day < datetime.datetime(2021, 7, 20):
+        return "AQMv5"
+    elif day < datetime.datetime(2024, 5, 14):
+        return "AQMv6"
+    else:
+        return "AQMv7"
+
+def create_naqfc_url_and_get_xr(day: datetime.date, cycle: int) -> xr.Dataset:
+    """
+    Data source: https://registry.opendata.aws/noaa-nws-naqfc-pds/?utm_source=chatgpt.com
+    Properties of data:
+        Data source is split by year:
+            beginning with AQMv5 in 2020, 
+            transitioning to AQMv6 on 20 July 2021, 
+            and to AQMv7 on 14 May 2024. 
+            The length of each forecast was 48 hours prior to the implementation of AQMv6
+        Only cycles 06 and 12 exist
     
-    # Constants for creating the full URL
-    blob_container = "https://noaa-nws-naqfc-pds.s3.amazonaws.com/AQMv6"
-    product = "ave_1hr_pm25_bc.227"  # 1-hour average PM2.5 concentration
+    Args:
+        day (datetime.date):
+            The date (UTC) of the forecast run.
+        cycle (int):
+            The initialization hour of the forecast [6, 12 UTC].
+            e.g., `cycle=12` corresponds to the 12Z model run.
+    Returns:
+        xr.Dataset:
+            An xarray Dataset containing NAQFC forecast for pmtf.
+    """
+    if cycle != 6 and cycle != 12:
+        raise ValueError(f"create_naqfc_url_and_get_xr: cycle must be either 6 or 12, but is {cycle}")
+    
+    # 1. Constants for creating the full URL
+    blob_container = "https://noaa-nws-naqfc-pds.s3.amazonaws.com"
+    AQM_version = get_AQM_version(day)
+    product = "ave_1hr_pm25_bc"
 
-    # Put it all together
-    file_name = f"aqm.t{cycle:02}z.{product}.grib2"
-    url = f"{blob_container}/cs.{day:%Y%m%d}/{file_name}"
+    file_name =  f"aqm.t{cycle:02}z.{product}.{day:%Y%m%d}.227.grib2"
+    url = f"{blob_container}/{AQM_version}/CS/{day:%Y%m%d}/{cycle:02}/{file_name}"
 
-    print(url)
-    # Download the file
+    # 2. Fetch grib data
     response = requests.get(url)
     with open(file_name, 'wb') as file:
         file.write(response.content)
-
-    print(f"Downloaded {file_name}")
 
     # Open the GRIB2 file
     ds = xr.open_dataset(file_name, engine='cfgrib')
