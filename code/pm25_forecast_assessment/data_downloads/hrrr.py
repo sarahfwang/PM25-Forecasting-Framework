6084bef2cc554d1d64a747b241cea338b4f723e7
sarahfwang <sfwang00@gmail.com>
2025-11-12 13:20:24 -0500

    hrrr data download bugs: data source no longer available; 'mdens' attr not found -> values under 'unknown' attr

--- code/pm25_forecast_assessment/data_downloads/hrrr.py
+++ code/pm25_forecast_assessment/data_downloads/hrrr.py
@@ -10,22 +10,44 @@ import requests
 from sklearn.metrics.pairwise import haversine_distances
 import xarray as xr
 
+def create_url_and_get_xr(day: datetime.date, cycle: int, leadtime: int) -> xr.Dataset:
+    """
+    Download and open a single HRRR surface forecast file (wrfsfcf) as an xarray Dataset.
+    Note: downloads for a single forecast may take on the order of 1 min.
 
-def create_url_and_get_xr(day: datetime.date, cycle: int, leadtime: int):
-    # Constants for creating the full URL
-    blob_container = "https://noaahrrr.blob.core.windows.net/hrrr"
+    Data source:
+        NOAA HRRR CONUS archive on AWS S3
+        https://noaa-hrrr-bdp-pds.s3.amazonaws.com
+
+    Args:
+        day (datetime.date):
+            The date (UTC) of the forecast run.
+        cycle (int):
+            The initialization hour of the forecast [0–23 UTC].
+            e.g., `cycle=12` corresponds to the 12Z model run.
+        leadtime (int):
+            The forecast hour relative to initialization [0–18].
+            e.g., `leadtime=3` retrieves the 3-hour forecast from that cycle.
+    Returns:
+        xr.Dataset:
+            An xarray Dataset containing HRRR surface-level forecast for MASSDEN.
+    """
+
+    # 1. Constants for creating the full URL
+    url_base = "https://noaa-hrrr-bdp-pds.s3.amazonaws.com"
     sector = "conus"
-    product = "wrfsfcf"  # 2D surface levels
+    cycle_string = f"{cycle:02}" # [0, 23] forecast start time
+    leadtime_string = f"{leadtime:02}" # [0, 18] number of hours past start time
+    file_path = f"hrrr.t{cycle_string}z.wrfsfcf{leadtime_string}.grib2"
 
-    # Put it all together
-    file_path = f"hrrr.t{cycle:02}z.{product}{leadtime:02}.grib2"
-    url = f"{blob_container}/hrrr.{day:%Y%m%d}/{sector}/{file_path}"
+    # 2. Grab .idx file for location of MASSDEN info in grib file
+    url = f"{url_base}/hrrr.{day:%Y%m%d}/{sector}/{file_path}"
     r = requests.get(f"{url}.idx")
     idx = r.text.splitlines()
 
     smk_line = [l for l in idx if "MASSDEN" in l][0].split(
         ":"
-    )  # [l for l in idx if "COLMD" in l][0].split(":")
+    )
     line_num = int(smk_line[0])
     range_start = smk_line[1]
     next_line = idx[line_num].split(":") if line_num < len(idx) else None
@@ -33,6 +55,7 @@ def create_url_and_get_xr(day: datetime.date, cycle: int, leadtime: int):
 
     file = tempfile.NamedTemporaryFile(prefix="tmp_", delete=False)
 
+    # 3. Using byte offsets, grab MASSDEN values from grib file
     headers = {"Range": f"bytes={range_start}-{range_end}"}
     resp = requests.get(url, headers=headers, stream=True)
 
@@ -41,14 +64,17 @@ def create_url_and_get_xr(day: datetime.date, cycle: int, leadtime: int):
 
     ds = xr.open_dataset(file.name, engine="cfgrib")
 
+    # sanity check
+    assert (len(ds.latitude.values) == len(ds.longitude.values) == len(ds.unknown.values))
+    
     return ds
 
-
+    
 def hrrr_data_download(date: datetime.date, cycle: int = 0) -> pd.DataFrame:
     def _download_hour(leadtime: int):
         ds = create_url_and_get_xr(date, cycle, leadtime)
         # Rescale entry of mdens to micrograms/m^2
-        mdens_rescaled = ds.mdens * 1e9
+        mdens_rescaled = ds.unknown * 1e9
         # Get the latitude and longitude values
         lats = ds.latitude.values
         lons = ds.longitude.values
